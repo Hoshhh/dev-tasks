61b18f85fd8393286a893256be6b4c7224404d4c
Hoshhh <nyxious123@live.com>
2022-12-18 15:24:00 -0500

    Added redux auth slice

---
+++ frontend/src/features/auth/authService.js
@@ -0,0 +1,20 @@
+import axious from 'axios'
+
+const API_URL = '/api/users/'
+
+//Register User
+const register = async(userData) => {
+    const response = await axious.post(API_URL, userData)
+
+    if (response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data))
+    }
+
+    return response.data
+}
+
+const authService = {
+    register
+}
+
+export default authService
