6977f21c916f1ce3e48b5a3403d15b677ba07f25
Hoshhh <nyxious123@live.com>
2022-12-24 23:07:27 -0500

    test 2

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,10 @@ app.use('/api/users', require('./routes/userRoutes'));
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'public', 'index.html')))
+    app.get('*', (req,res) => res.sendFile(path.join(__dirname, '../frontend/build/index.html'),
+    function (err) {
+        res.status(500).send(err)
+    }))
 } else {
     app.get('/', (req,res) => res.send('Not set to production'))
 }
